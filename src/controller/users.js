0746e1a4289316db74ecf51f7844636b744e2924
DaminboevM <m701rizo@gmail.com>
2025-04-22 22:31:20 +0500

    database yangilandi

--- src/controller/users.js
+++ src/controller/users.js
@@ -8,9 +8,35 @@ const history = fs.readFileSync(path.join(process.cwd(), "database/history.json"
 // Muhammadrizo
 const POST = (req, res) => {
     try {        
-        if(!req.body)throw Error("body bosh")
+        if(!req.body)throw Error("body not found")
         const {fromUserId, toUserId, amount} = req.body
-        console.log(fromUserId, toUserId, amount)
+
+        const user = users.find(user => user.userId == fromUserId)
+        const toUser = users.find(el => el.transactionId == toUserId)
+
+        if(!user || !toUser) throw Error("user not found")
+        if(user.balance < amount) throw Error("balanse not enough")
+
+        user.balance -= amount
+        toUser.balance += amount
+
+        const date = new Date()
+        const newHistory = {
+            transactionId: history.length ? history.at(-1).transactionId + 1 : 1,
+            fromUserId,
+            toUserId,
+            amount,
+            date: date.toLocaleDateString('en-GB',{year:'numeric',month:'short',day:"2-digit",hour:'2-digit',minute:'2-digit',second:'2-digit'})
+        }
+
+        history.push(newHistory)
+        fs.writeFileSync(path.join(process.cwd(), "database/history.json"), JSON.stringify(history, null, 2))
+
+        res.status(200).send({
+            status: 200,
+            message: "succsess",
+        })
+
     } catch (error) {
         res.send(error.message)
     }
